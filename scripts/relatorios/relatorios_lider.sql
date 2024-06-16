8148095f9085a7cd25b7ef14d96fae1cce20f760
KSalmaze <105123464+KSalmaze@users.noreply.github.com>
2024-06-16 18:34:50 +0000

    correçao dos selects de lider

--- scripts/relatorios/relatorios_lider.sql
+++ scripts/relatorios/relatorios_lider.sql
@@ -20,23 +20,6 @@ END Relatorios_Lider_de_Faccao;
 /
 
 CREATE OR REPLACE PACKAGE BODY Relatorios_Lider_de_Faccao AS
-
-    -- Verifica se o lider possui faccões
-    /*FUNCTION Verificar_Faccoes_Lider(
-     lider_logado IN lider%ROWTYPE)
-     RETURN BOOLEAN IS
-     v_faccao NUMBER;
-     BEGIN 
-        
-        SELECT COUNT(*) INTO v_faccao
-        FROM Lider L JOIN Faccao F;
-     
-        IF v_faccao > 1 THEN
-            RETURN FALSE;
-        ELSE    
-            RETURN TRUE;
-        END IF;
-     END Verificar_Faccoes_Lider;*/
      
     PROCEDURE Comunidades_por_Nacao(
      lider_logado IN lider%ROWTYPE) IS
@@ -49,17 +32,19 @@ CREATE OR REPLACE PACKAGE BODY Relatorios_Lider_de_Faccao AS
         PA.Especie AS Especie,
         C.QTD_Habitantes AS QTD_Habitantes
         FROM Lider L 
-        JOIN Faccao F ON L.CPI = F.Lider
+        JOIN Faccao F ON L.CPI = F.Lider AND L.CPI = lider_logado.CPI
         JOIN Nacao_Faccao NF ON NF.Faccao = F.Nome
         JOIN Nacao N ON N.Nome = NF.Nacao
-        JOIN Dominancia D ON D.Nacao = N.Nome AND DATA_INI <= TRUNC(SYSDATE) AND
-                    (DATA_FIM >= TRUNC(SYSDATE) OR DATA_FIM IS NULL)
+        JOIN Dominancia D ON D.Nacao = N.Nome AND D.DATA_INI <= TRUNC(SYSDATE) AND
+                    (D.DATA_FIM >= TRUNC(SYSDATE) OR D.DATA_FIM IS NULL)
         JOIN Planeta P ON P.Id_Astro = D.Planeta
         JOIN Orbita_Planeta OP ON OP.Planeta = P.Id_Astro
         JOIN Estrela E ON E.Id_Estrela = OP.Estrela
         JOIN Sistema S ON S.Estrela = E.Id_Estrela
         JOIN Participa PA ON PA.Faccao = F.Nome
         JOIN Comunidade C ON C.Especie = PA.Especie AND C.Nome = PA.Comunidade
+        JOIN Habitacao H ON H.Comunidade = C.Nome AND H.Especie = C.Especie AND H.DATA_INI <= TRUNC(SYSDATE) AND
+                    (H.DATA_FIM >= TRUNC(SYSDATE) OR H.DATA_FIM IS NULL)
         ORDER BY N.Nome;
      END Comunidades_por_Nacao;
      
@@ -74,17 +59,19 @@ CREATE OR REPLACE PACKAGE BODY Relatorios_Lider_de_Faccao AS
         PA.Especie AS Especie,
         C.QTD_Habitantes AS QTD_Habitantes
         FROM Lider L 
-        JOIN Faccao F ON L.CPI = F.Lider
+        JOIN Faccao F ON L.CPI = F.Lider AND L.CPI = lider_logado.CPI
         JOIN Nacao_Faccao NF ON NF.Faccao = F.Nome
         JOIN Nacao N ON N.Nome = NF.Nacao
-        JOIN Dominancia D ON D.Nacao = N.Nome AND DATA_INI <= TRUNC(SYSDATE) AND
-                    (DATA_FIM >= TRUNC(SYSDATE) OR DATA_FIM IS NULL)
+        JOIN Dominancia D ON D.Nacao = N.Nome AND D.DATA_INI <= TRUNC(SYSDATE) AND
+                    (D.DATA_FIM >= TRUNC(SYSDATE) OR D.DATA_FIM IS NULL)
         JOIN Planeta P ON P.Id_Astro = D.Planeta
         JOIN Orbita_Planeta OP ON OP.Planeta = P.Id_Astro
         JOIN Estrela E ON E.Id_Estrela = OP.Estrela
         JOIN Sistema S ON S.Estrela = E.Id_Estrela
         JOIN Participa PA ON PA.Faccao = F.Nome
         JOIN Comunidade C ON C.Especie = PA.Especie AND C.Nome = PA.Comunidade
+        JOIN Habitacao H ON H.Comunidade = C.Nome AND H.Especie = C.Especie AND H.DATA_INI <= TRUNC(SYSDATE) AND
+                    (H.DATA_FIM >= TRUNC(SYSDATE) OR H.DATA_FIM IS NULL)
         ORDER BY PA.Especie;
      END Comunidades_por_especie;
 
@@ -99,17 +86,19 @@ CREATE OR REPLACE PACKAGE BODY Relatorios_Lider_de_Faccao AS
         PA.Especie AS Especie,
         C.QTD_Habitantes AS QTD_Habitantes
         FROM Lider L 
-        JOIN Faccao F ON L.CPI = F.Lider
+        JOIN Faccao F ON L.CPI = F.Lider AND L.CPI = lider_logado.CPI
         JOIN Nacao_Faccao NF ON NF.Faccao = F.Nome
         JOIN Nacao N ON N.Nome = NF.Nacao
-        JOIN Dominancia D ON D.Nacao = N.Nome AND DATA_INI <= TRUNC(SYSDATE) AND
-                    (DATA_FIM >= TRUNC(SYSDATE) OR DATA_FIM IS NULL)
+        JOIN Dominancia D ON D.Nacao = N.Nome AND D.DATA_INI <= TRUNC(SYSDATE) AND
+                    (D.DATA_FIM >= TRUNC(SYSDATE) OR D.DATA_FIM IS NULL)
         JOIN Planeta P ON P.Id_Astro = D.Planeta
         JOIN Orbita_Planeta OP ON OP.Planeta = P.Id_Astro
         JOIN Estrela E ON E.Id_Estrela = OP.Estrela
         JOIN Sistema S ON S.Estrela = E.Id_Estrela
         JOIN Participa PA ON PA.Faccao = F.Nome
         JOIN Comunidade C ON C.Especie = PA.Especie AND C.Nome = PA.Comunidade
+        JOIN Habitacao H ON H.Comunidade = C.Nome AND H.Especie = C.Especie AND H.DATA_INI <= TRUNC(SYSDATE) AND
+                    (H.DATA_FIM >= TRUNC(SYSDATE) OR H.DATA_FIM IS NULL)
         ORDER BY P.Id_Astro;
      END Comunidades_por_planeta;
 
@@ -124,19 +113,22 @@ CREATE OR REPLACE PACKAGE BODY Relatorios_Lider_de_Faccao AS
         PA.Especie AS Especie,
         C.QTD_Habitantes AS QTD_Habitantes
         FROM Lider L 
-        JOIN Faccao F ON L.CPI = F.Lider
+        JOIN Faccao F ON L.CPI = F.Lider AND L.CPI = lider_logado.CPI
         JOIN Nacao_Faccao NF ON NF.Faccao = F.Nome
         JOIN Nacao N ON N.Nome = NF.Nacao
-        JOIN Dominancia D ON D.Nacao = N.Nome AND DATA_INI <= TRUNC(SYSDATE) AND
-                    (DATA_FIM >= TRUNC(SYSDATE) OR DATA_FIM IS NULL)
+        JOIN Dominancia D ON D.Nacao = N.Nome AND D.DATA_INI <= TRUNC(SYSDATE) AND
+                    (D.DATA_FIM >= TRUNC(SYSDATE) OR D.DATA_FIM IS NULL)
         JOIN Planeta P ON P.Id_Astro = D.Planeta
         JOIN Orbita_Planeta OP ON OP.Planeta = P.Id_Astro
         JOIN Estrela E ON E.Id_Estrela = OP.Estrela
         JOIN Sistema S ON S.Estrela = E.Id_Estrela
         JOIN Participa PA ON PA.Faccao = F.Nome
         JOIN Comunidade C ON C.Especie = PA.Especie AND C.Nome = PA.Comunidade
+        JOIN Habitacao H ON H.Comunidade = C.Nome AND H.Especie = C.Especie AND H.DATA_INI <= TRUNC(SYSDATE) AND
+                    (H.DATA_FIM >= TRUNC(SYSDATE) OR H.DATA_FIM IS NULL)
         ORDER BY S.Nome;
      END Comunidades_por_sistema;
+     
 END Relatorios_Lider_de_Faccao;
 
 /
