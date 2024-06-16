f445d53ebbf99711bb5cfd62d97f2933e192d408
KSalmaze <105123464+KSalmaze@users.noreply.github.com>
2024-06-16 20:51:52 +0000

    Retorno alterado

--- scripts/relatorios/relatorios_lider.sql
+++ scripts/relatorios/relatorios_lider.sql
@@ -1,134 +1,71 @@
+CREATE OR REPLACE TYPE Comunidade_Record AS OBJECT (
+    Nome            VARCHAR2(100),
+    Nacao           VARCHAR2(100),
+    Planeta         VARCHAR2(100),
+    Sistema         VARCHAR2(100),
+    Especie         VARCHAR2(100),
+    QTD_Habitantes  NUMBER
+);
+
+/
+
+CREATE OR REPLACE TYPE Comunidade_Table AS TABLE OF Comunidade_Record;
+
+/
 
 CREATE OR REPLACE PACKAGE Relatorios_Lider_de_Faccao AS
     
-    E_acesso_negado EXCEPTION;
-    
-    PROCEDURE Comunidades_por_Nacao(
-     lider_logado IN lider%ROWTYPE);
-        
-    PROCEDURE Comunidades_por_especie(
-     lider_logado IN lider%ROWTYPE);
-     
-    PROCEDURE Comunidades_por_planeta(
-     lider_logado IN lider%ROWTYPE);
-     
-    PROCEDURE Comunidades_por_sistema(
-     lider_logado IN lider%ROWTYPE);
-    
+    -- Exceções personalizadas
+    Sem_Dados EXCEPTION;
+    Nao_Eh_Algo_Valido_Para_Ordenar EXCEPTION;
+
+    -- Declaração da função que retorna uma tabela de registros
+    FUNCTION Gerar_Relatorio(lider_logado IN lider%ROWTYPE, ordenar_por VARCHAR2) 
+    RETURN Comunidade_Table PIPELINED;
+
 END Relatorios_Lider_de_Faccao;
 
 /
 
 CREATE OR REPLACE PACKAGE BODY Relatorios_Lider_de_Faccao AS
-     
-    PROCEDURE Comunidades_por_Nacao(
-     lider_logado IN lider%ROWTYPE) IS
-     
-     BEGIN
-        SELECT C.NOME AS Nome,
-        N.Nome AS Nacao,
-        P.Id_Astro AS Planeta,
-        S.Nome AS Sistema,
-        PA.Especie AS Especie,
-        C.QTD_Habitantes AS QTD_Habitantes
-        FROM Lider L 
-        JOIN Faccao F ON L.CPI = F.Lider AND L.CPI = lider_logado.CPI
-        JOIN Nacao_Faccao NF ON NF.Faccao = F.Nome
-        JOIN Nacao N ON N.Nome = NF.Nacao
-        JOIN Dominancia D ON D.Nacao = N.Nome AND D.DATA_INI <= TRUNC(SYSDATE) AND
-                    (D.DATA_FIM >= TRUNC(SYSDATE) OR D.DATA_FIM IS NULL)
-        JOIN Planeta P ON P.Id_Astro = D.Planeta
-        JOIN Orbita_Planeta OP ON OP.Planeta = P.Id_Astro
-        JOIN Estrela E ON E.Id_Estrela = OP.Estrela
-        JOIN Sistema S ON S.Estrela = E.Id_Estrela
-        JOIN Participa PA ON PA.Faccao = F.Nome
-        JOIN Comunidade C ON C.Especie = PA.Especie AND C.Nome = PA.Comunidade
-        JOIN Habitacao H ON H.Comunidade = C.Nome AND H.Especie = C.Especie AND H.DATA_INI <= TRUNC(SYSDATE) AND
-                    (H.DATA_FIM >= TRUNC(SYSDATE) OR H.DATA_FIM IS NULL)
-        ORDER BY N.Nome;
-     END Comunidades_por_Nacao;
-     
-    PROCEDURE Comunidades_por_especie(
-     lider_logado IN lider%ROWTYPE) IS
-     
-     BEGIN
-        SELECT C.NOME AS Nome,
-        N.Nome AS Nacao,
-        P.Id_Astro AS Planeta,
-        S.Nome AS Sistema,
-        PA.Especie AS Especie,
-        C.QTD_Habitantes AS QTD_Habitantes
-        FROM Lider L 
-        JOIN Faccao F ON L.CPI = F.Lider AND L.CPI = lider_logado.CPI
-        JOIN Nacao_Faccao NF ON NF.Faccao = F.Nome
-        JOIN Nacao N ON N.Nome = NF.Nacao
-        JOIN Dominancia D ON D.Nacao = N.Nome AND D.DATA_INI <= TRUNC(SYSDATE) AND
-                    (D.DATA_FIM >= TRUNC(SYSDATE) OR D.DATA_FIM IS NULL)
-        JOIN Planeta P ON P.Id_Astro = D.Planeta
-        JOIN Orbita_Planeta OP ON OP.Planeta = P.Id_Astro
-        JOIN Estrela E ON E.Id_Estrela = OP.Estrela
-        JOIN Sistema S ON S.Estrela = E.Id_Estrela
-        JOIN Participa PA ON PA.Faccao = F.Nome
-        JOIN Comunidade C ON C.Especie = PA.Especie AND C.Nome = PA.Comunidade
-        JOIN Habitacao H ON H.Comunidade = C.Nome AND H.Especie = C.Especie AND H.DATA_INI <= TRUNC(SYSDATE) AND
-                    (H.DATA_FIM >= TRUNC(SYSDATE) OR H.DATA_FIM IS NULL)
-        ORDER BY PA.Especie;
-     END Comunidades_por_especie;
 
-    PROCEDURE Comunidades_por_planeta(
-     lider_logado IN lider%ROWTYPE) IS
-     
-     BEGIN
-        SELECT C.NOME AS Nome,
-        N.Nome AS Nacao,
-        P.Id_Astro AS Planeta,
-        S.Nome AS Sistema,
-        PA.Especie AS Especie,
-        C.QTD_Habitantes AS QTD_Habitantes
-        FROM Lider L 
-        JOIN Faccao F ON L.CPI = F.Lider AND L.CPI = lider_logado.CPI
-        JOIN Nacao_Faccao NF ON NF.Faccao = F.Nome
-        JOIN Nacao N ON N.Nome = NF.Nacao
-        JOIN Dominancia D ON D.Nacao = N.Nome AND D.DATA_INI <= TRUNC(SYSDATE) AND
-                    (D.DATA_FIM >= TRUNC(SYSDATE) OR D.DATA_FIM IS NULL)
-        JOIN Planeta P ON P.Id_Astro = D.Planeta
-        JOIN Orbita_Planeta OP ON OP.Planeta = P.Id_Astro
-        JOIN Estrela E ON E.Id_Estrela = OP.Estrela
-        JOIN Sistema S ON S.Estrela = E.Id_Estrela
-        JOIN Participa PA ON PA.Faccao = F.Nome
-        JOIN Comunidade C ON C.Especie = PA.Especie AND C.Nome = PA.Comunidade
-        JOIN Habitacao H ON H.Comunidade = C.Nome AND H.Especie = C.Especie AND H.DATA_INI <= TRUNC(SYSDATE) AND
-                    (H.DATA_FIM >= TRUNC(SYSDATE) OR H.DATA_FIM IS NULL)
-        ORDER BY P.Id_Astro;
-     END Comunidades_por_planeta;
+    -- Função para gerar o relatório
+    FUNCTION Gerar_Relatorio(lider_logado IN lider%ROWTYPE, ordenar_por VARCHAR2) RETURN Comunidade_Table PIPELINED IS
+        CURSOR c_communities IS
+            SELECT 
+                C.NOME AS Nome,
+                N.Nome AS Nacao,
+                P.Id_Astro AS Planeta,
+                S.Nome AS Sistema,
+                PA.Especie AS Especie,
+                C.QTD_Habitantes AS QTD_Habitantes
+            FROM Lider L 
+            JOIN Faccao F ON L.CPI = F.Lider AND L.CPI = lider_logado.CPI
+            JOIN Nacao_Faccao NF ON NF.Faccao = F.Nome
+            JOIN Nacao N ON N.Nome = NF.Nacao
+            JOIN Dominancia D ON D.Nacao = N.Nome AND D.DATA_INI <= TRUNC(SYSDATE) AND
+                            (D.DATA_FIM >= TRUNC(SYSDATE) OR D.DATA_FIM IS NULL)
+            JOIN Planeta P ON P.Id_Astro = D.Planeta
+            JOIN Orbita_Planeta OP ON OP.Planeta = P.Id_Astro
+            JOIN Estrela E ON E.Id_Estrela = OP.Estrela
+            JOIN Sistema S ON S.Estrela = E.Id_Estrela
+            JOIN Participa PA ON PA.Faccao = F.Nome
+            JOIN Comunidade C ON C.Especie = PA.Especie AND C.Nome = PA.Comunidade
+            JOIN Habitacao H ON H.Comunidade = C.Nome AND H.Planeta = P.Id_Astro AND H.Especie = C.Especie AND H.DATA_INI <= TRUNC(SYSDATE) AND
+                            (H.DATA_FIM >= TRUNC(SYSDATE) OR H.DATA_FIM IS NULL)
+            ORDER BY CASE UPPER(ordenar_por)
+                        WHEN 'NACAO' THEN N.Nome
+                        WHEN 'ESPECIE' THEN PA.Especie
+                        WHEN 'PLANETA' THEN P.Id_Astro
+                        WHEN 'SISTEMA' THEN S.Nome
+                        ELSE NULL
+                    END;
+    BEGIN
+        FOR rec IN c_communities LOOP
+            PIPE ROW (Comunidade_Record(rec.Nome, rec.Nacao, rec.Planeta, rec.Sistema, rec.Especie, rec.QTD_Habitantes));
+        END LOOP;
+    END Gerar_Relatorio;
 
-    PROCEDURE Comunidades_por_sistema(
-     lider_logado IN lider%ROWTYPE) IS
-     
-     BEGIN
-        SELECT C.NOME AS Nome,
-        N.Nome AS Nacao,
-        P.Id_Astro AS Planeta,
-        S.Nome AS Sistema,
-        PA.Especie AS Especie,
-        C.QTD_Habitantes AS QTD_Habitantes
-        FROM Lider L 
-        JOIN Faccao F ON L.CPI = F.Lider AND L.CPI = lider_logado.CPI
-        JOIN Nacao_Faccao NF ON NF.Faccao = F.Nome
-        JOIN Nacao N ON N.Nome = NF.Nacao
-        JOIN Dominancia D ON D.Nacao = N.Nome AND D.DATA_INI <= TRUNC(SYSDATE) AND
-                    (D.DATA_FIM >= TRUNC(SYSDATE) OR D.DATA_FIM IS NULL)
-        JOIN Planeta P ON P.Id_Astro = D.Planeta
-        JOIN Orbita_Planeta OP ON OP.Planeta = P.Id_Astro
-        JOIN Estrela E ON E.Id_Estrela = OP.Estrela
-        JOIN Sistema S ON S.Estrela = E.Id_Estrela
-        JOIN Participa PA ON PA.Faccao = F.Nome
-        JOIN Comunidade C ON C.Especie = PA.Especie AND C.Nome = PA.Comunidade
-        JOIN Habitacao H ON H.Comunidade = C.Nome AND H.Especie = C.Especie AND H.DATA_INI <= TRUNC(SYSDATE) AND
-                    (H.DATA_FIM >= TRUNC(SYSDATE) OR H.DATA_FIM IS NULL)
-        ORDER BY S.Nome;
-     END Comunidades_por_sistema;
-     
 END Relatorios_Lider_de_Faccao;
 
 /
