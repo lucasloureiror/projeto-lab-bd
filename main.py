fab1c72d58f9bfd804f62100166a002aef4f70c2
Lucas Loureiro Rodrigues <prolucasloureiro@gmail.com>
2024-06-16 15:24:33 -0300

    feat: funcionalidade do cookie

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI, Request, Form, Response
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import repository.connection
+import json
 from models import Usuario
 
 app = FastAPI()
@@ -16,14 +17,27 @@ async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/submit/")
-async def handle_form(request: Request, username: str = Form(...), password: str = Form(...)):
+async def handle_form(request: Request, response: Response, username: str = Form(...), password: str = Form(...)):
     
     resultado = await repository.connection.check_credentials(username, password)
 
     # Login bem-sucedido se a função retornar um ID válido
     if isinstance(resultado, Usuario):
         print("Usuario logado: {", resultado.user_id, ",", resultado.username, ",", resultado.cargo, ",", resultado.eh_lider_faccao, "}")
+
+        # Serializar o objeto resultado para JSON
+        resultado_json = json.dumps({
+            "user_id": resultado.user_id,
+            "username": resultado.username,
+            "cargo": resultado.cargo,
+            "eh_lider_faccao": resultado.eh_lider_faccao
+        })
+
+        # Definir o cookie
+        response.set_cookie(key="user_data", value=resultado_json)
+
         
         return templates.TemplateResponse("index.html", {"request": request, "message": "Login realizado com sucesso"})
     else:
         return templates.TemplateResponse("index.html", {"request": request, "message": resultado})
+
