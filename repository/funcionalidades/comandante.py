be26e6a91b27defd41b63cc8abce9460ced69dc5
Livia <liviafausto03@gmail.com>
2024-06-18 18:52:25 -0300

    fix: mensagens de erro do comandante

--- repository/funcionalidades/comandante.py
+++ repository/funcionalidades/comandante.py
@@ -27,26 +27,29 @@ def incluir_propria_nacao(nome_federacao:str, usuario:Usuario):
         except oracledb.DatabaseError as e:
             error, = e.args
             if error.code == 20001 and ("Lider nao encontrado" in error.message):
-                mensagem = "ERRO: Líder não encontrado."
+                mensagem_erro = "ERRO: Líder não encontrado."
             elif error.code == 20001:
-                mensagem = "ERRO: Federação não encontrada."
+                mensagem_erro = "ERRO: Federação não encontrada."
             elif error.code == 20004:
-                mensagem = "ERRO: O nome da federação que será incluída não pode ser nulo."
+                mensagem_erro = "ERRO: O nome da federação que será incluída não pode ser nulo."
             elif error.code == 20005 and ("ja faz parte dessa federacao" in error.message):
-                mensagem = "ERRO: Sua nação já faz parte dessa federação."
+                mensagem_erro = "ERRO: Sua nação já faz parte dessa federação."
             elif error.code == 20005:
-                mensagem = "ERRO: Sua nação está atualmente incluída em outra federação. Exclua essa associação e tente novamente."
+                mensagem_erro = "ERRO: Sua nação está atualmente incluída em outra federação. Exclua essa associação e tente novamente."
+            elif error.code == 12899 and ("maximum: 15" in error.message):
+                mensagem_erro = "ERRO: O nome de federação informado é muito grande. Informe um nome com até 15 caracteres e tente novamente."
             else:
-                mensagem = f"{error.message}"
+                mensagem_erro = f"{error.message}"
             
             connection.rollback()
 
-            mensagem_log = f"Tentativa de incluir a nação do líder na federação '{nome_federacao}' --> {mensagem}"
+            mensagem_log = f"Tentativa de incluir a nação do líder na federação '{nome_federacao}' --> {mensagem_erro}"
+            mensagem_log = utils.ajustar_mensagem_log(mensagem_log)
             cursor.callproc(NOVO_LOG, [usuario.user_id, mensagem_log])
             connection.commit()
 
             print(error.message)
-            return mensagem
+            return mensagem_erro
         
         finally:
             cursor.close()
@@ -74,23 +77,25 @@ def excluir_propria_nacao(nome_federacao:str, usuario:Usuario):
         except oracledb.DatabaseError as e:
             error, = e.args
             if error.code == 20001:
-                mensagem = "ERRO: Líder não encontrado."
+                mensagem_erro = "ERRO: Líder não encontrado."
             elif error.code == 20004:
-                mensagem = "ERRO: O nome da federação que será excluída não pode ser nulo."
+                mensagem_erro = "ERRO: O nome da federação que será excluída não pode ser nulo."
             elif error.code == 20005 and ("nao faz parte de nenhuma federacao" in error.message):
-                mensagem = "ERRO: Sua nação não faz parte de nenhuma federação."
+                mensagem_erro = "ERRO: Sua nação não faz parte de nenhuma federação."
             elif error.code == 20005:
-                mensagem = f"ERRO: Sua nação não está incluída na federação '{nome_federacao}'."
+                mensagem_erro = f"ERRO: Sua nação não está incluída na federação '{nome_federacao}'."
             else:
-                mensagem = f"{error.message}"
+                mensagem_erro = f"{error.message}"
+            
             connection.rollback()
 
-            mensagem_log = f"Tentativa de excluir a nação do líder da federação '{nome_federacao}' --> {mensagem}"
+            mensagem_log = f"Tentativa de excluir a nação do líder da federação '{nome_federacao}' --> {mensagem_erro}"
+            mensagem_log = utils.ajustar_mensagem_log(mensagem_log)
             cursor.callproc(NOVO_LOG, [usuario.user_id, mensagem_log])
             connection.commit()
 
             print(error.message)
-            return mensagem
+            return mensagem_erro
         
         finally:
             cursor.close()
@@ -170,24 +175,27 @@ def inserir_dominancia(id_planeta:str, data_ini:utils.Data, usuario:Usuario):
         except oracledb.DatabaseError as e:
             error, = e.args
             if error.code == 20001 and ("Lider nao encontrado" in error.message):
-                mensagem = "ERRO: Líder não encontrado."
+                mensagem_erro = "ERRO: Líder não encontrado."
             elif error.code == 20001:
-                mensagem = "ERRO: Planeta não encontrado."
+                mensagem_erro = "ERRO: Planeta não encontrado."
             elif error.code == 20004:
-                mensagem = "ERRO: O id do planeta e a data de início não podem ser nulos."
+                mensagem_erro = "ERRO: O id do planeta e a data de início não podem ser nulos."
             elif error.code == 20005:
-                mensagem = "ERRO: Esse planeta já esta sendo dominado."
+                mensagem_erro = "ERRO: Esse planeta já esta sendo dominado."
+            elif error.code == 12899 and ("maximum: 15" in error.message):
+                mensagem_erro = "ERRO: O id de planeta informado é muito grande. Informe um id com até 15 caracteres e tente novamente."
             else:
-                mensagem = f"{error.message}"
+                mensagem_erro = f"{error.message}"
 
             connection.rollback()
 
-            mensagem_log = f"Tentativa de inserir dominância da nação do líder sobre o planeta '{id_planeta}' --> {mensagem}"
+            mensagem_log = f"Tentativa de inserir dominância da nação do líder sobre o planeta '{id_planeta}' --> {mensagem_erro}"
+            mensagem_log = utils.ajustar_mensagem_log(mensagem_log)
             cursor.callproc(NOVO_LOG, [usuario.user_id, mensagem_log])
             connection.commit()
 
             print(error.message)
-            return mensagem
+            return mensagem_erro
         
         finally:
             cursor.close()
