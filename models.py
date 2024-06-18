e8a8bf6420c2bfbdb29544e7d202d3b8d1915ba7
Lucas Loureiro Rodrigues <prolucasloureiro@gmail.com>
2024-06-18 15:03:02 -0300

    feat: relatorios funcionando

--- models.py
+++ models.py
@@ -18,3 +18,33 @@ class Estrela:
 
     def __str__(self):
         return f"Estrela: [{self.id}, {self.nome}, {self.classificacao}, {self.massa}, {self.x}, {self.y}, {self.z}]"
+    
+
+class RelatorioLider:
+    nome: str
+    nacao: str
+    planeta: str
+    sistema: str
+    especie: str
+    qtd_habitantes: int
+
+    def __init__(self, nome: str, nacao: str, planeta: str, sistema: str, especie: str, qtd_habitantes: int):
+        self.nome = nome
+        self.nacao = nacao
+        self.planeta = planeta
+        self.sistema = sistema
+        self.especie = especie
+        self.qtd_habitantes = qtd_habitantes
+
+    def __repr__(self):
+        return f"RelatorioLider(nome={self.nome}, nacao={self.nacao}, planeta={self.planeta}, sistema={self.sistema}, especie={self.especie}, qtd_habitantes={self.qtd_habitantes})"
+
+    def to_dict(self):
+        return {
+            "nome": self.nome,
+            "nacao": self.nacao,
+            "planeta": self.planeta,
+            "sistema": self.sistema,
+            "especie": self.especie,
+            "qtd_habitantes": self.qtd_habitantes
+        }
